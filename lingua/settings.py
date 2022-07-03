e469b636be12a04e4a4596732cc4af3eca45fad7
anamarijamatevska1 <anamarijamatevska@outlook.com>
2022-07-03 14:06:38 +0200

    Secret key updated

--- lingua/settings.py
+++ lingua/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-klihly30d--4ci$h9&&#fdh+glq1%f5*xq=62hez2n0llx05rv'
+SECRET_KEY = 'django-insecure-klihly30d--4ci$h9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
